0519bb3958a41e4ea492f32374e3f96b16672899
fg44317 <grimssys@gmail.com>
2022-08-12 12:15:28 +0200

    checkers added

---
+++ Games/Checkers/Checkers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace checkers{
+    class Checkers{
+        static void Main(string[] args){
+            Game game = new Game();
+            Board board1 = new Board();
+            char[,] board = board1.board1;
+            board1.startingPosition(board);
+            board1.printBoard(board); 
+            game.makeMove(board);
+        }
+    }
+}
