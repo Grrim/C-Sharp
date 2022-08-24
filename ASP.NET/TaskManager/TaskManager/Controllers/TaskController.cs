8db7af6d9dc1e9e38c38107de1853ce7122a59b4
fg44317 <grimssys@gmail.com>
2022-08-24 17:51:58 +0200

    task manager added

---
+++ ASP.NET/TaskManager/TaskManager/Controllers/TaskController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+    public class TaskController : Controller
+    {
+        private static IList<TaskModel> tasks = new List<TaskModel>()
+        {
+            new TaskModel(){ TaskId = 1, TaskName = "Wizyta u lekarza", Description =  "Godzina 17:00", Done = false},
+            new TaskModel(){ TaskId = 2, TaskName = "Wyjść z psem", Description =  "Godzina 19:00", Done = false},
+        };
+
+        // GET: TaskController
+        public ActionResult Index()
+        {
+            return View(tasks.Where(x => !x.Done));
+        }
+
+        // GET: TaskController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(tasks.FirstOrDefault(x => x.TaskId == id));
+        }
+
+        // GET: TaskController/Create
+        public ActionResult Create()
+        {
+            return View(new TaskModel());
+        }
+
+        // POST: TaskController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TaskModel taskModel)
+        {
+            taskModel.TaskId = tasks.Count + 1;
+            tasks.Add(taskModel);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: TaskController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(tasks.FirstOrDefault(x => x.TaskId == id));
+        }
+
+        // POST: TaskController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, TaskModel taskModel)
+        {
+            TaskModel task = tasks.FirstOrDefault(x => x.TaskId == id);
+            task.TaskName = taskModel.TaskName;
+            task.Description = taskModel.Description;
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: TaskController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(tasks.FirstOrDefault(x => x.TaskId == id));
+        }
+
+        // POST: TaskController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, TaskModel taskModel)
+        {
+            TaskModel task = tasks.FirstOrDefault(x => x.TaskId == id);
+            tasks.Remove(task);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: TaskController/Done/5
+        public ActionResult Done(int id)
+        {
+            TaskModel task = tasks.FirstOrDefault(x => x.TaskId == id);
+            task.Done = true;
+
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
---
+++ ASP.NET/TaskManager/TaskManager/Models/TaskModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace TaskManager.Models
+{
+    public class TaskModel
+    {
+        public int TaskId { get; set; }
+        [DisplayName("Nazwa")]
+        public string? TaskName { get; set; }
+        [DisplayName("Opis")]
+        public string? Description { get; set; }
+        public bool Done { get; set; }
+    }
+}
